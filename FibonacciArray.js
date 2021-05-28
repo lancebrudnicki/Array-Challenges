90df9e9a55a1af8857801584d423aeaa70cc8d19
lancebrudnicki <lancebrudnicki@gmail.com>
2021-05-27 21:13:58 -0600

    done

--- FibonacciArray.js
+++ FibonacciArray.js
@@ -6,7 +6,7 @@ function fibonacciArray(n) {
     var fibArr = [0, 1];
 
     for(var i=2; i < n; i++){
-        fibArr.push(fibArr[i-1] + fibArr[i-2])
+        fibArr.push(fibArr[i-2] + fibArr[i-1])
     }
 
     return fibArr;
--- arrayrev.js
+++ arrayrev.js
@@ -8,6 +8,24 @@ function reverse(arr) {
 
     return arr;
 }
-   
 var result = reverse(["a", "b", "c", "d", "e"]);
 console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+
+
+function reverseArray(arr) {
+    var newArray = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+    newArray.push(arr[i]);
+    }
+    return newArray;
+}
+
+function reverseARray(arr){
+
+    for(var i =0; i<arr.length/2; i++){
+        var temp = arr[i];
+        arr[i] = arr[arr.length -1-i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr;
+}
